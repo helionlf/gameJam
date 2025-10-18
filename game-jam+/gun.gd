47f5f01ef84412645a1901423dd08c4d6f10d258
Infernape333 <116040431+Infernape333@users.noreply.github.com>
2025-10-18 08:49:18 -0300

    Raycast

--- game-jam+/gun.gd
+++ game-jam+/gun.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-var cena_projetil = preload("res://projetil.tscn") 
+signal arma_desapareceu
+
+@onready var raycast: RayCast2D = $RayCast2D
+@onready var timer_cooldown = $Timer
+@onready var timer_desaparecer = $TimerDesaparecer
+@onready var animated_sprite = $AnimatedSprite2D
 
 var municao: int = 7
 var pode_atirar: bool = true
@@ -15,6 +20,10 @@ func equipar_arma():
 	municao = 7
 	pode_atirar = true
 
+func desequipar():
+	visible = false
+	set_process(false)
+
 func _process(delta):
 	if Input.is_action_just_pressed("Atirar") and pode_atirar and municao > 0:
 		atirar()
@@ -23,30 +32,37 @@ func atirar():
 	pode_atirar = false
 	municao -= 1
 	
-	$Timer.start()
-	$AnimatedSprite2D.play("Atirando")
+	timer_cooldown.start()
+	animated_sprite.play("Atirando")
 	
-	var direcao_tiro = Vector2.RIGHT 
-	
+	var direcao_arma = 1
 	if get_parent().scale.x < 0:
-		direcao_tiro = Vector2.LEFT
+		direcao_arma = -1
+		
+	raycast.target_position.x = 1000 * direcao_arma
 	
-	var novo_projetil = cena_projetil.instantiate() 
-	get_tree().root.add_child(novo_projetil)
-	novo_projetil.iniciar($Marker2D.global_position, direcao_tiro)
+	raycast.force_raycast_update()
+	
+	if raycast.is_colliding():
+		var alvo = raycast.get_collider()
+		
+		if alvo.is_in_group("inimigo"):
+			pass
+			#
+			# COLOQUE SEU CÓDIGO DE DANO AQUI
+			# Ex: alvo.sofrer_dano(10)
+			#
 	
 	if municao == 0:
-		$AnimatedSprite2D.play("Desapearing")
-		await get_tree().create_timer(.3)
-		$TimerDesaparecer.start()
-
-func desequipar():
-	visible = false
-	set_process(false)
-
-func _on_timer_desaparecer_timeout():
-	visible = false
-	set_process(false)
+		timer_desaparecer.start()
 
 func _on_timer_timeout():
 	pode_atirar = true
+
+func _on_timer_desaparecer_timeout():
+	$AnimatedSprite2D.play("Desapearing")
+	await get_tree().create_timer(0.3)
+	
+	visible = false
+	set_process(false)
+	arma_desapareceu.emit()
